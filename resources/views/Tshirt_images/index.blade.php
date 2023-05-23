904b7e36460bca853344a71a78b12f26a3823c31
Leandro Pereira <2211053@my.ipleiria.pt>
2023-05-23 20:43:11 +0100

    Alterações nos nomes de modelo e controler

--- resources/views/Tshirt_images/index.blade.php
+++ resources/views/Tshirt_images/index.blade.php
@@ -26,11 +26,11 @@
     </tr>
     </thead>
     <tbody>
-    @foreach ($tshirt_images as $image)
+    @foreach ($tshirtImages as $image)
         <tr>
             <td>{{ $image->name }}</td>
             <td>{{ $image->description }}</td>
-            <td><img src="{{ $image->image_url }}" alt="Imagem"></td>
+            <td><img src="{{ $image->fullTshirt_imageUrl }}"  alt="Imagem"></td>
 
         </tr>
     @endforeach
