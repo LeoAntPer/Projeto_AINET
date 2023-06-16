0696cf5b14d37c85d85b695c0f546ffb9f480c36
Joel Batista <joelbasim@gmail.com>
2023-06-16 17:49:09 +0000

    estetica do cart.show; remover um item do carrinho; diminuir quantidade de um item no carrinho; aumentar quantidade de um item no carrinho;

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -10,6 +10,7 @@ use Illuminate\View\View;
 
 class CartController extends Controller
 {
+    const ITEM_NOT_IN_CART = -1;
     public function show(): View
     {
         $cart = session('cart', []);
@@ -19,13 +20,28 @@ class CartController extends Controller
     public function addToCart(Request $request): RedirectResponse
     {
         try {
+            $tshirt_image_id = $request->input('imageID');
+            $color_code = $request->input('color');
+            $size = $request->input('size');
+            $cart = session('cart', []);
+
+            // Se ja existe um order_item igual apenas queremos aumentar a quantidade
+            $cartIndex = $this->isAlreadyInCart($tshirt_image_id, $color_code, $size);
+            if($cartIndex != self::ITEM_NOT_IN_CART){
+                $cart[$cartIndex]->qty++;
+                return back();
+            }
+
             // Create a temporary order_item (not in DB)
             $newOrderItem = new OrderItem();
-            $newOrderItem->tshirt_image_id = $request->input('imageID');
-            $newOrderItem->color_code = $request->input('color');
-            $newOrderItem->size = $request->input('size');
+            $newOrderItem->tshirt_image_id = $tshirt_image_id;
+            $newOrderItem->color_code = $color_code;
+            $newOrderItem->size = $size;
+            $newOrderItem->qty = 1;
             $cartId = session('cart_id', 0);
-            $cart = session('cart', []);
+
+
+            // Add order_item to cart
             $cart[$cartId] = $newOrderItem;
             $cartId++;
             $request->session()->put('cart', $cart);
@@ -36,10 +52,47 @@ class CartController extends Controller
         return back();
     }
 
+    public function removeFromCart(Request $request, int $cartIndex): RedirectResponse
+    {
+        $cart = session('cart', []);
+        $orderItem = $cart[$cartIndex];
+
+        // verificar se e para remover completamente do carrinho ou apenas baixar quantidade
+        $fullDelete = $request->input('fullDelete') ?? false;
+        if($fullDelete and $orderItem->qty > 1){
+            $orderItem->qty--;
+            return back();
+        }
+        // remover item do carrinho
+        unset($cart[$cartIndex]);
+        // se o carrinho esta vazio, refresh
+        if(count($cart) === 0){
+            self::destroy($request);
+        }
+        $request->session()->put('cart', $cart);
+        return back();
+    }
+
     public function destroy(Request $request): RedirectResponse
     {
         $request->session()->forget('cart');
         $request->session()->forget('cart_id');
         return back();
+    }
+
+    private function isAlreadyInCart($tshirt_image_id, $color_code, $size): int
+    {
+        $cart = session('cart', []);
+        foreach ($cart as $id => $orderItem){
+            if (
+                $orderItem->tshirt_image_id == $tshirt_image_id and
+                $orderItem->color_code == $color_code and
+                $orderItem->size == $size
+            )
+            {
+                return $id;
+            }
+        }
+        return -1;
     }
 }
--- resources/views/cart/show.blade.php
+++ resources/views/cart/show.blade.php
@@ -1,18 +1,65 @@
 @extends('template.layout')
 @section('titulo', 'Shopping cart')
+@section('subtitulo')
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item">Shopping cart</li>
+    </ol>
+@endsection
+
 @section('main')
-    @foreach($cart as $orderItem)
-        <img class="card-img-top img-fluid" src="{{ $orderItem->tshirtImage->fullTshirtImageUrl }}" style="background-color: #2f2f2f; width: 200px; height: 200px; align-content: center" alt="Imagem">
-        <p>Size: {{ $orderItem->size }}</p>
-        <p>Quantity: {{ $orderItem->qty }}</p>
-        <p>Price: {{ $orderItem->unit_price }}</p>
-        <p>Subtotal: {{ $orderItem->sub_total }}</p>
-        <p>Color code: {{ $orderItem->color_code }}</p>
-    @endforeach
-    <form action="{{ route('cart.destroy') }}" method="POST">
-        @csrf
-        @method('DELETE')
-        <button type="submit">Clear</button>
-    </form>
-<?php dump($cart) ?>
+    <table class="table">
+        <thead class="table-dark">
+        <tr>
+            <th>Image</th>
+            <th>Color</th>
+            <th>Size</th>
+            <th>Quantity</th>
+            <th>Unit Price</th>
+            <th>SubTotal</th>
+
+            <th class="button-icon-col"></th>
+            <th class="button-icon-col"></th>
+            <th class="button-icon-col"></th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach ($cart as $cartIndex => $orderItem)
+            <tr>
+                <td><img class="card-img-top img-fluid" src="{{ $orderItem->tshirtImage->fullTshirtImageUrl }}" style="background-color: #2f2f2f; width: 40px; height: 40px; align-content: center" alt="Imagem"></td>
+                <td><div style="background-color: {{ '#'.$orderItem->color_code }}; width: 40px; height: 40px; border-radius: 5px"></div></td>
+                <td>{{ $orderItem->size }}</td>
+                <td>{{ $orderItem->qty }}</td>
+                <td>{{ $orderItem->unit_price }}</td>
+                <td>{{ $orderItem->sub_total }}</td>
+
+                <td class="button-icon-col">
+                    <form action="{{ route('cart.add') }}" method="POST">
+                        @csrf
+                        <input type="hidden" name="imageID" value="{{ $orderItem->tshirt_image_id }}">
+                        <input type="hidden" name="color" value="{{ $orderItem->color_code }}">
+                        <input type="hidden" name="size" value="{{ $orderItem->size }}">
+                        <button type="submit" class="btn btn-dark"><i class="fa-solid fa-plus"></i></button>
+                    </form>
+                </td>
+                <td class="button-icon-col">
+                    <form method="POST" action="{{ route('cart.remove', ['cartIndex' => $cartIndex]) }}" class="button-icon-col">
+                        @csrf
+                        @method('DELETE')
+                        <input type="hidden" name="fullDelete" value="1">
+                        <button type="submit" name="delete" class="btn btn-secondary"><i class="fa-solid fa-minus"></i></button>
+                    </form>
+                </td>
+                <td>
+                    <form method="POST" action="{{ route('cart.remove', ['cartIndex' => $cartIndex]) }}" class="button-icon-col">
+                        @csrf
+                        @method('DELETE')
+                        <input type="hidden" name="fullDelete" value="0">
+                        <button type="submit" name="delete" class="btn btn-danger"><i class="fas fa-trash"></i></button>
+                    </form>
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+<!--?php dump($cart) ?-->
 @endsection
